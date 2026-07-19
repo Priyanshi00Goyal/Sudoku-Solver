7d63c9a64db0440bba98a6cc3212e2565edbbdea
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-19 15:22:33 +0530

    Add Sudoku class with game functionality

--- include/Sudoku.h
+++ include/Sudoku.h
@@ -1,1 +1,74 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+#include<string>
+using namespace std;
 
+class Sudoku
+{
+
+private:
+
+    int board[9][9];
+
+    int difficulty;
+
+
+    string playerName;
+
+    int gamesPlayed;
+
+    int gamesSolved;
+
+    int bestScore;
+
+
+public:
+
+    Sudoku();
+
+
+    void welcomeScreen();
+
+    void instructions();
+
+    void loadingAnimation();
+
+    void getPlayerDetails();
+
+    void showStatistics();
+
+
+    void savePlayerData();
+
+    void loadPlayerData();
+
+
+    void menu();
+
+
+    void selectDifficulty();
+
+    void displayBoard();
+
+
+    void startTimer();
+
+    void calculateScore();
+
+
+    void saveGame();
+
+    void loadGame();
+
+
+    bool isSafe(int row,int col,int num);
+
+    bool isValid();
+
+    bool solve();
+
+    void inputBoard();
+};
+
+
+#endif
